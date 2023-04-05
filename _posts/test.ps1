c77b9c50fc53965ed54102e8304f33af2fe3d890
Valeras Narbutas <valeras.narbutas@macaw.nl>
2023-04-06 00:58:15 +0300

    enter

--- _posts/test.ps1
+++
@@ -1,7 +0,0 @@
-m365 login --authType deviceCode
-
-# Get all users and their properties, add properties as needed with --properties and separate with comma
-m365 aad user list --properties "displayName,mail"
-
-# Get all users and their properties and export to csv
-m365 aad user list --output csv --properties "displayName,mail,givenName,jobTitle,mail,mobilePhone,officeLocation,preferredLanguage,surname,userPrincipalName,id" > users.csv
